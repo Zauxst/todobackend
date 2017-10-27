7f1cfcad0cf4626025a400e0ce271185329caefb
Nicolae ROSU <nrosu@pentalog.fr>
2017-10-27 19:08:21 +0300

    Configured the application to work

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
 from django.db import models
 
@@ -6,5 +6,5 @@ from django.db import models
 class TodoItem(models.Model):
     title = models.CharField(max_length=256, null=True, blank=True)
     completed = models.BooleanField(blank=True, default=False)
-    url = models.Charfield(max_length=256, null=True, blank=True)
+    url = models.CharField(max_length=256, null=True, blank=True)
     order = models.IntegerField(null=True, blank=True)

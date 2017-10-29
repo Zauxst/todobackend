0ea4a911afd453fd959f80a31e78041fb68b22a2
Zauxst <zauxst@gmail.com>
2017-10-29 22:08:31 +0200

    finished settings

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,11 @@
 
 . /appenv/bin/Activate
 
+# Download requirements to build cache
+pip download -d /build -r requirements_test.txt --no-input
+
 # Install application test requirements
-pip install -r requirements_test.txt
+pip install --no-index -f /build -r requirements_test.txt
 
 # Run test.sh arguments
 exec $@
